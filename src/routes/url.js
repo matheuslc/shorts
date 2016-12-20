b8577accdbbd2527de21f59f2bf6c089f848994a
Matheus Carmo <me@matheuslc.com>
2016-12-20 17:13:42 -0200

    feat(url): Register URL routes

--- src/routes/url.js
+++ src/routes/url.js
@@ -2,20 +2,27 @@ import express from 'express';
 import ShortUrlController from '../url/shortUrlController';
 import ShortUrlRepository from '../url/shortUrlRepository';
 
-const router = express.Router;
+const router = express.Router();
 
 router.get('/:shortUrlId', (req, res) => {
   const ShortUrlRepositoryInstance = new ShortUrlRepository();
-
   const Controller = new ShortUrlController(ShortUrlRepositoryInstance);
+
   Controller.getShortUrl(req, res);
 });
 
 router.delete('/:shortUrlId', (req, res) => {
   const ShortUrlRepositoryInstance = new ShortUrlRepository();
-
   const Controller = new ShortUrlController(ShortUrlRepositoryInstance);
+
   Controller.deleteShortUrl(req, res);
+});
+
+router.post('/:url', (req, res) => {
+  const ShortUrlRepositoryInstance = new ShortUrlRepository();
+  const Controller = new ShortUrlController(ShortUrlRepositoryInstance);
+
+  Controller.createShortUrl(req, res);
 });
 
 export default router;
