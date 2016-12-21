299f66a4a37906bd8c34fd8b823c383d0690c6b2
Matheus Carmo <me@matheuslc.com>
2016-12-21 14:04:28 -0200

    fix(test): Fix code coverage summary

---
+++ test/url/shortUrlController.spec.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+import ShortUrlController from '../../src/url/shortUrlController';
+import config from '../fixtures/config.json';
+
+describe('Short URL Controller', () => {
+  it('Should return', () => {
+  	chai.assert.typeOf('a', 'string');
+  });
+});
