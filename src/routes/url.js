d81f8bd5b4f785a360cd99e7fb2cd3d24f6c89be
Matheus Carmo <mematheuslc@gmail.com>
2017-02-24 13:36:22 -0300

    ref(shortUrlService): Fix dependency inversion mistake

--- src/routes/url.js
+++ src/routes/url.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import config from '../../resource/config/enviroment';
+import Shortener from '../url/shortener';
 import ShortUrlController from '../url/shortUrlController';
 import ShortUrlRepository from '../url/shortUrlRepository';
 import ShortUrlService from '../url/shortUrlService';
 
 const router = express.Router();
 const ShortUrlRepositoryInstance = new ShortUrlRepository();
-const ShortUrlServiceInstance = new ShortUrlService();
+const ShortUrlServiceInstance = new ShortUrlService(new Shortener(config.dictionary, config.shortUrlSize));
 
 router.get('/:shortUrlId', (req, res) => {
   const Controller = new ShortUrlController(ShortUrlRepositoryInstance, ShortUrlServiceInstance);
